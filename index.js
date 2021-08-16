286f5bd213ddb618c939145fff226b4b49193aaf
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-08-16 10:29:55 -0400

    finished tasks 4/5

--- index.js
+++ index.js
@@ -28,11 +28,14 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+    -Counter 1 is creating the variable inside of the inner scope while Counter2 creates the variable outside the function scope.
+
   2. Which of the two uses a closure? How can you tell?
+    -Counter1 uses a closure because in the function counter() it is reaching outwards to receive the variable count.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
 */
 
 // counter1 code
@@ -62,8 +65,9 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+   let score = Math.floor(Math.random() * 3)
+    return score;
 }
 
 
@@ -81,8 +85,17 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(callback, numberOfInnings){
+  const scoreFinal = {
+    Home: 0,
+    Away: 0
+  };
+  for(let i=0; i < numberOfInnings; i++){
+    scoreFinal.Home += callback();
+    scoreFinal.Away += callback();
+  }
+  console.log(scoreFinal)
+  return scoreFinal;
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,8 +103,15 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(callback) {
+  let inningScore= {
+    Home: 0,
+    Away: 0 
+  }
+  inningScore.Home += callback()
+  inningScore.Away += callback()
+
+  return inningScore;
 }
 
 
